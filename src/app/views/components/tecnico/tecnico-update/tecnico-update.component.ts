dac65ce8b34f5b31a19564dc1b53c21ea7fd23a3
alemcar <sandromuniz27@hotmail.com>
2023-03-21 10:03:57 -0400

    update angular v14

--- src/app/views/components/tecnico/tecnico-update/tecnico-update.component.ts
+++ src/app/views/components/tecnico/tecnico-update/tecnico-update.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { UntypedFormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Tecnico } from 'src/app/models/tecnico';
 import { TecnicoService } from 'src/app/services/tecnico.service';
@@ -19,9 +19,9 @@ export class TecnicoUpdateComponent implements OnInit {
     telefone: ''
   }
 
-  nome = new FormControl('', [Validators.minLength(5)]);
-  cpf = new FormControl('', [Validators.minLength(11)]);
-  telefone = new FormControl('', [Validators.minLength(11)]);
+  nome = new UntypedFormControl('', [Validators.minLength(5)]);
+  cpf = new UntypedFormControl('', [Validators.minLength(11)]);
+  telefone = new UntypedFormControl('', [Validators.minLength(11)]);
 
   constructor(
     private router: Router,
